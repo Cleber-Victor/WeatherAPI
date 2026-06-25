70f5e1c517d97a2fd467394bce4e358cfeba04ee
Cleber-victor <cvsj@cin.ufpe.br>
2026-06-25 11:13:53 -0300

    Integrating with External API

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-import { createServer } from "node:http";
+import "dotenv/config";
+import express from "express";
+import Redis from "ioredis";
 
-const hostname = "127.0.0.1";
+const redis = new Redis();
+
+const app = express();
 const port = 3000;
+const map1 = new Map();
+const apiKey = process.env.API_KEY;
 
-const server = createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  const header = req.headers;
-  let body = [];
-  req
-    .on("error", (err) => {
-      console.error(err);
-    })
-    .on("data", (chunk) => {
-      body.push(chunk);
-    })
-    .on("end", () => {
-      body = Buffer.concat(body).toString();
-      if (url === "/" && method === "GET") {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.write("Chegou");
-        res.end();
-      }
-    });
+map1.set("recife", "sunny");
+map1.set("sao Paulo", "cloudy");
+map1.set("londres", "rainy");
+
+async function getWeatherfromAPI(location) {
+  let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=${apiKey}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Erro HTTP: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Erro ao buscar os dados do clima:", error);
+  }
+}
+
+async function getWeather(location) {
+  const cacheKey = `weather:${location}`;
+
+  const cachedData = await redis.get(cacheKey);
+
+  if (cachedData) {
+    console.log("Servindo do Cache!");
+    return JSON.parse(cachedData);
+  }
+  console.log("Buscando da API...");
+  const apiData = await getWeatherfromAPI(location);
+
+  await redis.set(cacheKey, JSON.stringify(apiData), "EX", 3600);
+
+  return apiData;
+}
+
+app.get("/", (req, res) => {
+  res.json({ message: "chegou" });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/api/v1/weather/:city", async (req, res) => {
+  const location = req.params.city.toLowerCase();
+  /*if (map1.get(city) === undefined) {
+    res.status(404).send("Cidade inexistente");
+  } else {
+    res.json({ city: city, weather: map1[city] });
+  }
+    */
+  const weatherData = await getWeather(location);
+  res.json(weatherData);
+});
+
+app.listen(port, () => {
+  console.log(`Express server running at http://localhost:${port}`);
 });
